589894648db1a86c26286ea579fc31def3ffee4d
php4 <senthamizhselvi@aryvart.com>
2016-12-30 12:18:25 +0530

    #1 first_commit

---
+++ app/assets/images/JSONLint - The JSON Validator._files/blockfreead.php
@@ -0,0 +1,3 @@
+(function(){
+window.blockFreeAd = "<a href=\"\" class=\"cta\" data-ga=\"textad\" target=\"_blank\"><img src=\"data:image;base64,\" style=\"float: left;width: 60px;border: 1px solid #e1e1e1; border-radius: 2px;margin: 0 10px 0 0;\" /><strong></strong><br><div style=\"clear: both;width: 100%;display: block;\"></div></a>";
+})();
